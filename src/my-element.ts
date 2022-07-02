a7cd1b8e5b3e57d24e3b30ff7f2e76f9c6ebebbd
miwashutaro0611 <jacksumiwa0061@gmail.com>
2022-07-03 00:14:26 +0900

    chore: install eslint-plugin-import

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,5 +1,5 @@
-import { customElement, property } from 'lit/decorators.js'
 import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
 /**
  * An example element.
